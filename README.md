9ffe298002f6b3cabf3f00f33ed930549cb1637d
kizzanaome <naomekizzan@gmail.com>
2023-02-01 01:23:09 +0300

    create a fully functional Todo App

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Game-Zone-App
 This is a mobile application designed in react native
+
+Excited to share with you my first react native application.
+![](todoapp.gif)
+
--- game-zone-app/App.js
+++ game-zone-app/App.js
@@ -1,71 +1,80 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import { Alert, FlatList, StyleSheet, Text, TouchableNativeFeedback, View, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { 'name': 'shaun', id: 1 },
-    { 'name': 'yoshi', id: 2 },
-    { 'name': 'mario', id: 3 },
-    { 'name': 'luigi', id: 4 },
-    { 'name': 'peach', id: 5 },
-    { 'name': 'toad', id: 6 },
-    { 'name': 'bowser', id: 7 }
-  ])
 
-  const pressHandler = (id) => {
-    setPeople((prevPoople) => {
-      return prevPoople.filter(person => person.id != id)
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: 1 },
+    { text: 'create an app', key: 2 },
+    { text: 'play on the switch', key: 3 }
 
-    });
+  ]);
 
+  const pressHandler = (key) => {
+    setTodos((previousTodo) => {
+      return previousTodo.filter
+        (todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OPPS!', 'Todos must be over 3 chars long', [
+        { text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
   }
 
   return (
-    <View style={styles.container}>
-
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      {/* <ScrollView>
-        {people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
+    <TouchableNativeFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo
+            submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item}
+                  pressHandler={pressHandler} />
+              )
+              }
+            />
           </View>
-        )
-        )
-        }
-      </ScrollView> */}
-
-      <StatusBar style="auto" />
-    </View>
+        </View>
+      </View>
+    </TouchableNativeFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    paddingHorizontal: 20,
-    paddingTop: 40
+    backgroundColor: '#fff'
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24
+  content: {
+    flex: 1,
+    padding: 40,
+    marginTop: 20,
+  },
+  list: {
+    flex: 1,
+    marginTop: 20
   }
 
 });
